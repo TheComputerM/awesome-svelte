d46cc7395775f1510c36c7ad3a29259bb3b9c49c
TheComputerM <mudit.harshit99@outlook.com>
2020-09-30 01:38:01 +0530

    init

---
+++ generate.js
@@ -0,0 +1,56 @@
+const fs = require("fs");
+const md = require("json2md");
+const remark = require("remark");
+const toc = require("remark-toc");
+const resources = require("./index");
+
+md.converters.resource = (input) => {
+  let output = `[${input.text}](${input.link})`;
+  if (input.info) output += ` – ${input.info}`;
+  return output;
+};
+
+function process(data, depth = 1) {
+  const output = [];
+  for (let resource of data) {
+    if (resource.link) {
+      output.push({ resource });
+    } else {
+      output.push(Object.fromEntries([[`h${depth + 1}`, resource.text]]));
+      if (resource.items[0].link) {
+        output.push({ ul: process(resource.items) });
+      } else {
+        output.push(...process(resource.items, depth + 1));
+      }
+    }
+  }
+  return output;
+}
+
+const file = md([
+  { h1: "Awesome Svelte" },
+  {
+    img: {
+      title: "Awesome Svelte",
+      source:
+        "https://cdn.rawgit.com/sindresorhus/awesome/d7305f38d29fed78fa85652e3a63e154dd8e8829/media/badge.svg",
+    },
+  },
+  { blockquote: "⚡ A curated list of awesome Svelte resources" },
+  { h2: "Table Of Contents" },
+  ...process(resources),
+  {
+    img: {
+      title: "CC0",
+      source:
+        "https://i.creativecommons.org/p/zero/1.0/88x31.png",
+    },
+  },
+]);
+
+remark()
+  .use(toc)
+  .process(file, (err, content) => {
+    if (err) throw err;
+    fs.writeFileSync("README.md", String(content));
+  });
---
+++ index.js
@@ -0,0 +1,143 @@
+module.exports = [
+  {
+    text: "Resources",
+    items: [
+      {
+        text: "Official Resources",
+        items: [
+          { text: "Official Guide", link: "https://svelte.dev/tutorial" },
+          { text: "API Reference", link: "https://svelte.dev/docs" },
+          { text: "GitHub Repo", link: "https://github.com/sveltejs/svelte" },
+          {
+            text: "Changelog",
+            link: "https://github.com/sveltejs/svelte/blob/master/CHANGELOG.md",
+          },
+        ],
+      },
+      {
+        text: "Community",
+        items: [{ text: "Twitter", link: "https://twitter.com/sveltejs" }],
+      },
+      {
+        text: "Conferences",
+        items: [{ text: "Svelte Summit", link: "https://sveltesummit.com/" }],
+      },
+      {
+        text: "Podcasts",
+        items: [{ text: "Svelte Radio", link: "https://www.svelteradio.com/" }],
+      },
+      {
+        text: "Youtube Channels",
+        items: [
+          {
+            text: "Svelte Society",
+            link: "https://www.youtube.com/channel/UCZSr5B0l07JXK2FIeWA0-jw",
+          },
+        ],
+      },
+      {
+        text: "Tutorials",
+        items: [
+          {
+            text: "Getting Started with Svelte 3",
+            link:
+              "https://www.digitalocean.com/community/tutorials/getting-started-with-svelte-3",
+            info: "DigitalOcean",
+          },
+          {
+            text: "Building My First Svelte App",
+            link:
+              "https://scotch.io/tutorials/building-my-first-svelte-app-thoughts-and-impressions",
+            info: "Scotch.io",
+          },
+          {
+            text: "An Introduction to Actions",
+            link: "https://svelte.school/tutorials/introduction-to-actions",
+            info: "Svelte School",
+          },
+        ],
+      },
+      {
+        text: "Templates",
+        items: [
+          {
+            text: "sveltejs/template",
+            link: "https://github.com/sveltejs/template",
+            info:
+              "Template for building basic applications with Svelte with rollup",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "Integrations",
+    items: [
+      {
+        text: "Preprocessing",
+        items: [
+          {
+            text: "svelte-preprocess",
+            link: "https://github.com/sveltejs/svelte-preprocess",
+            info:
+              "A ✨ magical ✨ Svelte preprocessor with sensible defaults and support for: PostCSS, SCSS, Less, Stylus, Coffeescript, TypeScript, Pug and much more.",
+          },
+          {
+            text: "MDSveX",
+            link: "https://github.com/pngwn/MDsveX",
+            info: "A markdown preprocessor for Svelte.",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    text: "Components & Libraries",
+    items: [
+      {
+        text: "UI Libraries",
+        items: [
+          {
+            text: "Svelte Materialify",
+            link: "https://github.com/TheComputerM/svelte-materialify",
+            info:
+              "A Material UI Design Component library for Svelte heavily inspired by vuetify.",
+          },
+          {
+            text: "Sveltestrap",
+            link: "https://github.com/bestguy/sveltestrap",
+            info: "Bootstrap 4 components for Svelte",
+          },
+          {
+            text: "Smelte",
+            link: "https://github.com/matyunya/smelte",
+            info:
+              "UI framework with material components built with Svelte and Tailwind CSS",
+          },
+          {
+            text: "Svelte Material UI",
+            link: "https://github.com/hperrin/svelte-material-ui",
+            info: "Svelte Material UI Components",
+          },
+        ],
+      },
+      {
+        text: "UI Components",
+        items: [
+          {
+            text: "svelte-notifications",
+            link: "https://github.com/beyonk-adventures/svelte-notifications",
+            info:
+              "Svelte toast notifications component that can be used in any JS application",
+          },
+          {
+            text: "svelte-checkbox",
+            link: "https://github.com/HosseinShabani/svelte-checkbox",
+            info:
+              "A checkbox component for Svelte (Cool animation, Customizable).",
+          },
+        ],
+      },
+    ],
+  },
+];
